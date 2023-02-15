67eab14dec87385d3ed418950f0346409912541a
mowuj <mowuj92gmail.com>
2023-02-15 18:53:34 +0600

    agentListView and agentRetrieveView

--- realtor/views.py
+++ realtor/views.py
@@ -1,16 +1,16 @@
-# from django.shortcuts import render
-# from rest_framework import permissions
-# from .models import Agent
-# from .serializers import *
-# from rest_framework.generics import ListAPIView,ListCreateAPIView,RetrieveAPIView,RetrieveDestroyAPIView,RetrieveUpdateAPIView
-# # Create your views here.
+from django.shortcuts import render
+from rest_framework import permissions
+from .models import Agent
+from .serializers import *
+from rest_framework.generics import ListAPIView,ListCreateAPIView,RetrieveAPIView,RetrieveDestroyAPIView,RetrieveUpdateAPIView
+# Create your views here.
 
-# class AgentListView(ListAPIView):
-#     permission_classes=(permissions.AlloyAny)
-#     queryset=Agent.objects.all()
-#     serializer=AgentSerializer
-#     pagination_class=None
+class AgentListView(ListAPIView):
+    permission_classes=(permissions.AlloyAny)
+    queryset=Agent.objects.all()
+    serializer=AgentSerializer
+    pagination_class=None
 
-# class AgentDetailView(RetrieveAPIView):
-#     queryset=Agent.objects.all()
-#     serializer_class=AgentSerializer
+class AgentDetailView(RetrieveAPIView):
+    queryset=Agent.objects.all()
+    serializer_class=AgentSerializer
