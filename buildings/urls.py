f227e2fc8a05ce4011123f07de340c8e07085afc
mowuj <mowuj92gmail.com>
2023-02-16 19:20:09 +0600

    search api

--- buildings/urls.py
+++ buildings/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', HomeListAPIView.as_view(), name='home'),
+    path('search/', Search.as_view(), name='search'),
     path('<slug>/', HomeDetailAPIView.as_view(), name='home-detail'),
     path('images/<pk>/', ImageView.as_view(), name='images'),
 ]
--- buildings/views.py
+++ buildings/views.py
@@ -7,6 +7,7 @@ from rest_framework.generics import ListAPIView,RetrieveAPIView
 from rest_framework.response import Response
 from .models import *
 from .serializers import *
+from django.db.models import Q,query
 
 class HomeListAPIView(ListAPIView):
     permission_classes=(permissions.AllowAny,)
@@ -25,3 +26,14 @@ class ImageView(APIView):
         images=home.images.all()
         serializer=ImageSerializer(images,many=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
+
+class Search(APIView):
+    permission_classes=(permissions.AllowAny,)
+    def post(self,request,format=None):
+        data=self.request.data
+        str=data['str']
+        q=(Q(description__icontains=str)) | (Q(title__icontains=str))
+        queryset=Home.objects.filter(is_published=True)
+        queryset=queryset.filter(q)
+        serializer=HomeSerializer(queryset,many=True)
+        return Response(serializer.data)
